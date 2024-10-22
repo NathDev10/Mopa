0c890afc1bef0766cabfc1d538aa2924edb2da48
Nathan <nathan.prudhomme15@gmail.com>
2024-10-22 21:12:20 +0200

    new Modes

--- src/pages/NewHome.js
+++ src/pages/NewHome.js
@@ -20,12 +20,18 @@ export default function NewHome(){
   const topRef = useRef(null);
   var top = true;
   const navigate = useNavigate();
+  const [StoryType, setStoryType] = useState('');
 
   const [isPopupVisible, setIsPopupVisible] = useState(false);
-  const correctPassword = 'zizi'; // Remplacez par le mot de passe correct
 
-  const handleButtonClick = () => {
+  const handleButtonClickRomantique = () => {
     setIsPopupVisible(true);
+    setStoryType('romantique')
+  };
+
+  const handleButtonClickSauvage = () => {
+    setIsPopupVisible(true);
+    setStoryType('sauvage')
   };
 
   const handleClosePopup = () => {
@@ -39,7 +45,12 @@ export default function NewHome(){
         alert('Correct password.');
         // Appelez votre fonction ici
         setIsPopupVisible(false);
-        enterRomantic();
+        if(StoryType === 'romantique'){
+          enterRomantic();
+        }
+        if(StoryType === 'sauvage'){
+          enterHot();
+        }
       } else {
         alert('Incorrect password.');
       }
@@ -65,7 +76,7 @@ export default function NewHome(){
 
       try {
         console.log('Envoi de la requête au serveur...');
-        const response = await axios.post(adressBackend + '/generate', storyData);
+        const response = await axios.post(adressBackend + '/generate_romantique', storyData);
         //await axios.post('https://sheltered-cove-94091.herokuapp.com/generate_audio', {texte: response.data.texte}); 
         //navigation.navigate('MyStory' , {Text: response.data.texte});
         //const text = `Dans la lumière dorée du coucher de soleil, je me tenais au sommet de la Tour Eiffel, admirant la vue imprenable sur Paris. Soudain, j'ai senti une présence derrière moi. Je me suis retournée et j'ai été frappée par la vue de Manuel Ferrara, un homme français grand et musclé, aux cheveux bruns et aux yeux perçants. Son regard intense m'a fait frissonner, et j'ai senti une attraction immédiate. Manuel s'est approché lentement,
@@ -89,11 +100,43 @@ export default function NewHome(){
       }
       setLoading(false)
       };
-     const enterHot = () => {
-    //     document.documentElement.style.setProperty('--main-color', 'linear-gradient(90deg,#ff3131, #ff914d)');
-    //     document.documentElement.style.setProperty('--font-color', '#9f0000');
-    //     TODO
-    };
+     const enterHot = async () => {
+        document.documentElement.style.setProperty('--main-color', 'linear-gradient(90deg,#ff3131, #ff914d)');
+        document.documentElement.style.setProperty('--font-color', '#9f0000');
+        setLoading(true)
+        //Vérifier paramètre NAME DESCRITPION ET LIEU 
+        const storyData = {
+            nom: name,
+            description: description,
+            lieu: location
+        };
+
+        try {
+          console.log('Envoi de la requête au serveur...');
+          const response = await axios.post(adressBackend + '/generate_sauvage', storyData);
+          //await axios.post('https://sheltered-cove-94091.herokuapp.com/generate_audio', {texte: response.data.texte}); 
+          //navigation.navigate('MyStory' , {Text: response.data.texte});
+          //const text = `Dans la lumière dorée du coucher de soleil, je me tenais au sommet de la Tour Eiffel, admirant la vue imprenable sur Paris. Soudain, j'ai senti une présence derrière moi. Je me suis retournée et j'ai été frappée par la vue de Manuel Ferrara, un homme français grand et musclé, aux cheveux bruns et aux yeux perçants. Son regard intense m'a fait frissonner, et j'ai senti une attraction immédiate. Manuel s'est approché lentement,
+          // son sourire charmant révélant des fossettes séduisantes. Ses yeux ne quittaient pas les miens, et j'ai senti mon cœur battre de plus en plus fort. Il a levé la main et a doucement effleuré ma joue, son toucher électrisant faisant naître une étincelle entre nous. Je pouvais sentir son désir, aussi ardent que le mien. Nos lèvres se sont rencontrées dans un baiser passionné, gourmand et plein de promesses. Ses mains ont commencé à explorer mon corps, ses doigts habiles me faisant frissonner de plaisir. Je pouvais sentir son excitation, son désir pour moi, et cela n'a fait qu'attiser le mien. Manuel a commencé à me déshabiller lentement, ses yeux ne quittant pas les miens.
+          //  Chaque morceau de vêtement qui tombait dévoilait un peu plus ma peau, et je pouvais voir le désir brûler dans ses yeux. Sa respiration est devenue plus rapide, plus profonde, et je pouvais sentir son corps réagir à chaque caresse. Nos corps nus se sont enlacés, et nous avons commencé à explorer l'autre de manière plus intime. Manuel était un amant attentionné, répondant à chaque gémissement, à chaque frisson avec une précision experte. Ses mains, sa bouche, son corps tout entier semblait connaître mes désirs avant même que je ne les exprime. Le corps à corps était passionné, nos corps s'emboîtant parfaitement. Manuel variait l'intensité de ses caresses, me laissant toujours au bord de l'orgasme, mais jamais assez pour que je bascule. Son contrôle était impressionnant, et cela ne faisait qu'augmenter mon désir pour lui. Je pouvais sentir son désir, son excitation, et j'ai décidé que c'était le moment. J'ai guidé Manuel en moi, et nous avons tous les deux gémis de plaisir. La connexion entre nous était profonde, intense, et je pouvais sentir chaque partie de mon corps réagir à lui. Manuel cherchait à me combler, à me donner du plaisir, et je me suis abandonnée à lui, à son toucher, à son amour. Le rapport était intense, passionné, et je pouvais sentir l'orgasme monter en moi. Manuel a continué à me stimuler, à me faire monter, jusqu'à ce que je bascule finalement, un cri de plaisir s'échappant de mes lèvres. Manuel a suivi peu après, son corps se tendant et se détendant dans mes bras. Après, nous sommes restés enlacés, nos corps nus et transpirants se collant l'un à l'autre. La tension a doucement décru, remplacée par une vague d'apaisement et de satisfaction. Nous sommes restés ainsi, profitant de la connexion profonde que nous avions partagée, jusqu'à ce que le soleil se couche et que les lumières de Paris s'allument, éclairant notre amour.`
+          const responseaudio = await axios.post(adressBackend + '/generate_audio', {texte: response.data.texte}, {
+            responseType: 'blob', // Important pour recevoir un fichier blob
+          });
+
+          const url = URL.createObjectURL(responseaudio.data);
+          
+          navigate('/mystory', { state: { Text: response.data.texte, audioUrl: url }, });
+          //console.log('Réponse reçue du serveur:', response.data);
+          //const story
+          //console.log(response.data.texte);
+          //console.log(storyText);
+          //PEUT ETRE VERIFIER LA SORTIE DE LA REQUETE ? 
+          //setStory(storyText);
+        } catch (error) {
+          console.error('Erreur', error);
+        }
+        setLoading(false)
+      };
 
     const scrollToBottom = () => {
       if(top){
@@ -142,7 +185,7 @@ export default function NewHome(){
                   <input className="story-input" type="text" placeholder="Ex : une salle de classe, le Four Seasons de Miami, un hammam" onChange={e => setLocation(e.target.value)}/>
                 </label>
                 <div className="story-button-group">
-                  <button type="button" onClick={handleButtonClick} className="story-romantic-button">
+                  <button type="button" onClick={handleButtonClickRomantique} className="story-romantic-button">
                   <img src={heartEmoji} height={35} width={35}></img>
                     Je veux une histoire romantique
                   </button>
@@ -152,7 +195,7 @@ export default function NewHome(){
                       onSubmit={handleSubmitPassword}
                     />
                   )}
-                  <button type="button" onClick={enterHot}  className="story-hot-button">
+                  <button type="button" onClick={handleButtonClickSauvage}  className="story-hot-button">
                   <img src={FireEmoji} height={35} width={35}></img>
                     Je veux une histoire torride
                   </button>
